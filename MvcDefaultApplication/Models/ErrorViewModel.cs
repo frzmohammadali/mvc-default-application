b51b4034c5e525cc455c1bb943128083d54c6af0
Mohammadali Forouzesh <frz.mohammadali.me@outlook.com>
2025-09-02 23:20:15 +0200

    added project files

---
+++ MvcDefaultApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcDefaultApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
