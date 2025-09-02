a81d727e651467a1d0c0f504b80d66336c5ae891
Mohammadali Forouzesh <frz.mohammadali.me@outlook.com>
2025-09-03 01:20:36 +0200

    updated workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,3 +33,24 @@ variable "vm_size" {
   type        = string
   default     = "Standard_DS2_v2"
 }
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
